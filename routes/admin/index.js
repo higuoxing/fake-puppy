96ec599aaae1bb5cbf8f4f4f8dc40e929a490ed6
higuoxing@gmail.com <higuoxing@outlook.com>
2018-05-18 19:10:01 +0800

    front-end: done

--- routes/admin/index.js
+++ routes/admin/index.js
@@ -2,4 +2,6 @@ module.exports = (app)=> {
   app.use('/admin/login' ,   require('./login' ));
   app.use('/admin/panel' ,   require('./panel' ));
   app.use('/admin/qrcode',   require('./qrcode'));
+  app.use('/admin/device',   require('./device'));
+  app.use('/admin/user'  ,   require('./user'  ));
 }
---
+++ routes/admin/user.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const router = express.Router();
+const check_admin = require('../../utils/admin/login').check_admin;
+const check_admin_login = require('../../middlewares/check_login').check_admin_login;
+
+router.get('/', async (req, res, next) => {
+  if (req.session.user === 'admin') {
+    res.render('admin/user');
+  } else {
+    res.render('admin/login');
+  }
+});
+
+
+module.exports = router
