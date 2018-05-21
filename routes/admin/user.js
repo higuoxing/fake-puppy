85ce3db2cb284362c2b64d8ae156b9f0bf83a3c7
higuoxing@gmail.com <higuoxing@outlook.com>
2018-05-21 14:40:37 +0800

    add: realtime plot

--- routes/admin/user.js
+++ routes/admin/user.js
@@ -5,16 +5,19 @@ const check_admin = require('../../utils/admin/login').check_admin;
 const check_admin_login = require('../../middlewares/check_login').check_admin_login;
 const _socket_conf = require('../../configs/default').socket_conf;
 
+// get active users
 router.get('/active', check_admin_login, async (req, res, next) => {
   let _users = await user_model.find({ state: 'activate' }).exec();
   res.render('admin/user', { users: _users, socket_conf: _socket_conf });
 });
 
+// get all users
 router.get('/all', check_admin_login, async (req, res, next) => {
   let _users = await user_model.find({ '$or': [{ state: 'activate' }, { state: 'pending' }] }).exec();
   res.render('admin/user', { users: _users, socket_conf: _socket_conf });
 });
 
+// get users of some device
 router.get('/device', check_admin_login, async (req, res, next) => {
   let _gw_id = req.query.gw_id;
   let _users = await user_model.find({ gw_id: _gw_id }).exec();
