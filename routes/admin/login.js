1aae2cd2d2ed985f2f7d2a2d7b8fb4cbbf00969b
higuoxing@gmail.com <higuoxing@outlook.com>
2018-05-31 00:48:31 +0800

    fix: password encrpt

--- routes/admin/login.js
+++ routes/admin/login.js
@@ -1,6 +1,7 @@
-const express           = require('express');
-const router            = express.Router();
-const check_admin_exist = require('../../utils/db/access/admin').check_admin_exist;
+const express            = require('express');
+const router             = express.Router();
+const check_admin_exist  = require('../../utils/db/access/admin').check_admin_exist;
+const get_the_only_admin = require('../../utils/db/access/admin').get_the_only_admin;
 
 router.get('/', async (req, res, next) => {
   if (req.session.user) {
@@ -9,7 +10,11 @@ router.get('/', async (req, res, next) => {
     // not log in
     let _check_admin_exist = await check_admin_exist();
     if (_check_admin_exist) {
-      res.render('admin/login');
+      let salt = (await get_the_only_admin()).salt;
+      res.render('admin/login', {
+        sess_id : req.sessionID,
+        salt    : salt
+      });
     } else {
       res.redirect('/admin/register');
     }
--- utils/admin/login.js
+++ utils/admin/login.js
@@ -1,14 +1,17 @@
-const admin_model = require('../db/model/admin');
-const crypto = require('crypto');
+const admin_model        = require('../db/model/admin');
+const crypto             = require('crypto');
+const check_amdin_exist  = require('../db/access/admin').check_admin_exist;
+const get_the_only_admin = require('../db/access/admin').get_the_only_admin;
 
 
 const _check_admin = async (user) => {
   const md5 = crypto.createHash('md5');
-  let admin = await admin_model.findOne({ username: user.username });
-  if (admin) {
+  let check_admin = await check_amdin_exist();
+  if (check_admin) {
     // admin exist
-    let _check_sum = md5.update(user.password + admin.salt).digest('hex').toString();
-    if (_check_sum === admin.password) {
+    let admin = await get_the_only_admin();
+    let _check_sum = md5.update(admin.password + user.sess_id).digest('hex').toString();
+    if (_check_sum === user.password) {
       // login successful
       return 'auth';
     } else {
